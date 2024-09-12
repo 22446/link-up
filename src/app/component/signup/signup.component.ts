e7fbce66acee5caecc4e8a7e67c6bf37fd4c2125
mazin safwat <mazin.safwat34@gmail.com>
2024-09-12 19:01:19 +0300

    dateType

--- src/app/component/signup/signup.component.ts
+++ src/app/component/signup/signup.component.ts
@@ -13,7 +13,7 @@ export class SignupComponent {
    _SignUpServices=inject(AuthService)
    _FormBuilder=inject(FormBuilder)
    _Router=inject(Router)
-  RegisterGroup:FormGroup=this._FormBuilder.group({
+    RegisterGroup:FormGroup=this._FormBuilder.group({
     name:[null,[Validators.required,Validators.minLength(3),Validators.maxLength(20)]],
     email : [null,[Validators.required,Validators.email]],
     password : [null,[Validators.required,Validators.pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)]],
